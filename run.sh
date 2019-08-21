7d2e41ab150b16013f784731df841177bd1ccc19
opqrstuvcut <vikz2713@gmail.com>
2019-08-22 00:11:52 +0900

    sed修正

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo alias vim=nvim >> ~/.bashrc
 . ~/.bashrc
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh 
 sh ./installer.sh ~/.cache/dein
-sed -i -e "s/HOMEPATH/"$HOME ./init.vim
+sed -e "s/HOMEPATH/"$HOME ./init.vim
 mkdir -p ~/.config/nvim
 ln -s ./init.vim ~/.config/nvim/init.vim
 
