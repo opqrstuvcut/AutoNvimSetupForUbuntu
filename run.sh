f921c1be618971bb9b7f51ba236582268a8a952f
opqrstuvcut <vikz2713@gmail.com>
2019-08-21 15:38:25 +0000

    正しく動作するように修正

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
-apt install software-properties-common
-add-apt-repository ppa:neovim-ppa/unstable
+apt install software-properties-common -y
+add-apt-repository ppa:neovim-ppa/unstable -y
 apt update -y
 apt install neovim -y
 echo alias vim=nvim >> ~/.bashrc
-. ~/.bashrc
+source ~/.bashrc
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh 
 sh ./installer.sh ~/.cache/dein
-sed -e "s/HOMEPATH/"$HOME ./init.vim
+sed -i -e "s#HOMEPATH#"$HOME"#g" ./init.vim
 mkdir -p ~/.config/nvim
-ln -s ./init.vim ~/.config/nvim/init.vim
+cp ./init.vim ~/.config/nvim/init.vim
 
 if type "pip3" > /dev/null 2>&1; then
-    pip3 install pyneovim
+    pip3 install pynvim
 else
-    pip install pyneovim
+    pip install pynvim
 fi
 
-curl -y -sL install-node.now.sh/lts | bash
+curl -sL install-node.now.sh/lts | bash
